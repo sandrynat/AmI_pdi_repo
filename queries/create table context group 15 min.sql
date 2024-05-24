0679d90a4906680a19953dcafbd4b0ac9d80ea70
Sandra Aguilar <70612462+sandrynat@users.noreply.github.com>
2024-05-24 01:16:30 +0100

    query 15 min

---
+++ queries/create table context group 15 min.sql
@@ -0,0 +1,27 @@
+CREATE TABLE context_dataset_stage_15min AS
+SELECT DATE_BIN('15 minutes', time_t, TIMESTAMP '2001-01-01') as time_t_15,
+avg(location_lat) location_lat,
+avg(location_lon) location_lon,
+max(wifi_connected) wifi_connected,
+avg(sensor_light_mean) sensor_light_mean,
+avg(sensor_accelerometer_x_mean) sensor_accelerometer_x_mean,
+avg(sensor_accelerometer_y_mean) sensor_accelerometer_y_mean,
+avg(sensor_accelerometer_z_mean) sensor_accelerometer_z_mean,
+avg(sensor_gravity_x_mean) sensor_gravity_x_mean,
+avg(sensor_gravity_y_mean) sensor_gravity_y_mean,
+avg(sensor_gravity_z_mean) sensor_gravity_z_mean,
+avg(sensor_gyroscope_x_mean) sensor_gyroscope_x_mean,
+avg(sensor_gyroscope_y_mean) sensor_gyroscope_y_mean,
+avg(sensor_gyroscope_z_mean) sensor_gyroscope_z_mean,
+avg(sensor_linear_acc_x_mean) sensor_linear_acc_x_mean,
+avg(sensor_linear_acc_y_mean) sensor_linear_acc_y_mean,
+avg(sensor_linear_acc_z_mean) sensor_linear_acc_z_mean,
+avg(sensor_rotation_vec_x_mean) sensor_rotation_vec_x_mean,
+avg(sensor_rotation_vec_y_mean) sensor_rotation_vec_y_mean,
+avg(sensor_rotation_vec_z_mean) sensor_rotation_vec_z_mean,
+avg(sensor_proximity_mean) sensor_proximity_mean,
+STRING_AGG (label, ';') label,
+STRING_AGG (activity_label, ';') activity_label,
+uid from
+context_dataset_stage
+group by time_t_15,uid, label order by time_t_15;
